40fd50623ad95d93a182f2a55d5fa34b93dc8adb
Eduardo <eduardo.almeida@terceirizados.medgrupo.com.br>
2022-07-02 12:50:48 -0300

    reset password token tests

--- test/users/passwords.spec.ts
+++ test/users/passwords.spec.ts
@@ -7,7 +7,7 @@ import supertest from 'supertest'
 const BASE_URL = `http://${process.env.HOST}:${process.env.PORT}`
 
 test.group('Passwords', (group) => {
-  test.only('it should send an email with a link to reset password', async (assert) => {
+  test('it should send an email with a link to reset password', async (assert) => {
     const user = await UserFactory.create()
 
     Mail.trap((message) => {
@@ -32,6 +32,21 @@ test.group('Passwords', (group) => {
       .expect(204)
 
     Mail.restore()
+  })
+
+  test.only('it should create a reset password token', async (assert) => {
+    const user = await UserFactory.create()
+
+    await supertest(BASE_URL)
+      .post('/forgot-password')
+      .send({
+        email: user.email,
+        resetPasswordUrl: 'url',
+      })
+      .expect(204)
+
+    const tokens = await user.related('tokens').query()
+    assert.isNotEmpty(tokens)
   })
 
   group.beforeEach(async () => {
