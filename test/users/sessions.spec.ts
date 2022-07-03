252ba3c4f5852044d52fefa406d2626f9c3ed9f5
Eduardo <eduardo.almeida@terceirizados.medgrupo.com.br>
2022-07-03 13:09:41 -0300

    return token on response and tests for token returned

--- test/users/sessions.spec.ts
+++ test/users/sessions.spec.ts
@@ -6,7 +6,7 @@ import { UserFactory } from './../../database/factories/index'
 const BASE_URL = `http://${process.env.HOST}:${process.env.PORT}`
 
 test.group('Session', (group) => {
-  test.only('it should authenticate an user', async (assert) => {
+  test('it should authenticate an user', async (assert) => {
     const plainPassword = 'teste123'
     const { id, email } = await UserFactory.merge({ password: plainPassword }).create()
 
@@ -19,6 +19,22 @@ test.group('Session', (group) => {
       .expect(201)
 
     assert.isDefined(body.user, 'User undefined')
+    assert.equal(body.user.id, id, 'Id not match')
+  })
+
+  test.only('it should return an api token when session is created', async (assert) => {
+    const plainPassword = 'teste123'
+    const { id, email } = await UserFactory.merge({ password: plainPassword }).create()
+
+    const { body } = await supertest(BASE_URL)
+      .post('/sessions')
+      .send({
+        email,
+        password: plainPassword,
+      })
+      .expect(201)
+
+    assert.isDefined(body.token, 'Token undefined')
     assert.equal(body.user.id, id, 'Id not match')
   })
 
